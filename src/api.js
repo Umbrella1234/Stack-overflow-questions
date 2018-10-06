51c563a57b16896c676546d392da5c69cb5b217d
Umbrella1234 <rozimark96@mail.ru>
2018-10-06 19:59:53 +0300

    removed page size

--- src/api.js
+++ src/api.js
@@ -2,12 +2,11 @@ import axios from "axios";
 
 const API_URL = "https://api.stackexchange.com/2.2/";
 
-export const getQuestions = (page, pageSize = 15) =>
+export const getQuestions = (page) =>
   axios.get(`${API_URL}/questions`, {
     params: {
       site: "stackoverflow",
       page,
-      pageSize,
       order: "desc",
       sort: "creation",
       filter: 'withbody'
